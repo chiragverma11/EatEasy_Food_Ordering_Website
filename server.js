4775151c28b056db5768ea67f3e2c05854d1826e
Chirag Verma <chiragverma1013@gmail.com>
2023-03-12 18:09:58 +0530

    Added CSS and Wrote HTML/EJS for HomePage

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.static('public'))
 app.use('/css', express.static(__dirname + '/public/css/'))
 app.use('/js', express.static(__dirname + '/public/js/'))
 app.use('/img', express.static(__dirname + '/public/img/'))
+// app.use('/assets', express.static(__dirname + '/assets/'))
 
 //Express Parser
 app.use(express.json())
